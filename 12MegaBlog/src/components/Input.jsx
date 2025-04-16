d896317b1e4b08d346c2c7b4798742770841810a
SanketMole <sanketmole78@gmail.com>
2025-04-16 23:52:40 +0530

    Button component

--- 12MegaBlog/src/components/Input.jsx
+++ 12MegaBlog/src/components/Input.jsx
@@ -1,6 +1,6 @@
 import React, {useId} from 'react'
 
-const Input = React.forwardRef(function Input({
+const Input = React.forwardRef( function Input({
     label,
     type = "text",
     className = "",
@@ -10,17 +10,18 @@ const Input = React.forwardRef(function Input({
     return (
         <div className='w-full'>
             {label && <label 
-            className='block mb-1' 
+            className='inline-block mb-1 pl-1' 
             htmlFor={id}>
                 {label}
             </label>
             }
-            <input type={text}
+            <input
+            type={type}
             className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
             ref={ref}
             {...props}
             id={id}
-            />    
+            />
         </div>
     )
 })
--- 12MegaBlog/src/components/PostCard.jsx
+++ 12MegaBlog/src/components/PostCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import appwriteService from "../appwrite/config"
-import {Link} from ''
+import {Link} from 'react-router-dom'
 
 function PostCard({$id, title, featuredImage}) {
     return (
